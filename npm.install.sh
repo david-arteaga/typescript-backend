7290691db53dbff1da7112a36102a25c4c3ca422
David Arteaga <daap99@gmail.com>
2018-05-15 10:15:34 -0500

    add ms dependency to npm install script

--- npm.install.sh
+++ npm.install.sh
@@ -1,3 +1,3 @@
-npm i --save express cors jsonwebtoken passport passport-facebook-token passport-jwt morgan body-parser debug typescript source-map-support knex pg bluebird bookshelf bookshelf-cascade-delete inversify reflect-metadata pm2 bcryptjs
+npm i --save express cors jsonwebtoken passport passport-facebook-token passport-jwt morgan body-parser debug typescript source-map-support knex pg bluebird bookshelf bookshelf-cascade-delete inversify reflect-metadata pm2 bcryptjs ms
 
 npm i --save-dev @types/express @types/cors @types/jsonwebtoken @types/passport @types/passport-facebook-token @types/passport-jwt @types/morgan @types/body-parser @types/debug @types/node @types/source-map-support @types/knex @types/bluebird supervisor @types/bookshelf @types/bcryptjs tslint-eslint-rules
